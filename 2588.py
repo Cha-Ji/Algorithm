550cbf7deb9253c0147b716c827a91b4df4c6acc
차지원 <chajiwon100785@gmail.com>
2019-11-05 20:40:20 +0900

    base

---
+++ 2588.py
@@ -0,0 +1,10 @@
+#Ex_2588
+# 출력
+
+first=input()
+second=input()
+
+print(int(first)*int(second[2]))
+print(int(first)*int(second[1]))
+print(int(first)*int(second[0]))
+print(int(first)*int(second))
