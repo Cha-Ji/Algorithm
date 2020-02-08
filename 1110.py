6793a55094983328dda4490a4e4093c04ddbff37
차지원 <chajiwon100785@gmail.com>
2020-02-08 13:29:52 +0900

    Ex_15649 [back_tracking] try~

--- 1110.py
+++
@@ -1,7 +0,0 @@
-#02
-import sys
-A_list=[]
-
-for i in range(int(input())) :
-    A_list.append(int(sys.stdin.readlines()))
-print(A_list)
--- test.py
+++
@@ -1,9 +0,0 @@
-import collections
-ct = collections.Counter()
-with open('test.txt','r') as f:
-    for li in f:
-        ct.update(li.rstrip())
-
-for item,cnt in ct.most_common():
-    print("%s : %d"%(item,cnt))
-print(ct.most_common())
