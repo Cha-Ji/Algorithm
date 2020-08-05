309aebf0f0af6f71842deb29c3bc57cbc1781dbb
차지원 <chajiwon100785@gmail.com>
2020-08-05 15:09:05 +0900

    LeetCode _20.08_Week1 : Vaild Palindrome

---
+++ LeetCode/20_08_1/Vaild_Palindrome.py
@@ -0,0 +1,19 @@
+# 2020.08.05_August Challenge
+# palindrome : 회문
+class Solution:
+    def checkAscii(self,str) -> bool:    # only upper
+        if 48 <= ord(str) <= 57:
+            return False
+        elif 65 <= ord(str) <= 90:
+            return False
+        else:
+            return True
+
+    def isPalindrome(self, s: str) -> bool:
+        s = s.upper()
+        for i in s:
+            if self.checkAscii(self.i):
+                s = s.replace(i,"").strip()
+        return s == s[::-1]
+print(Solution.isPalindrome(Solution,"A man, a plan, a canal: Panama"))
+
