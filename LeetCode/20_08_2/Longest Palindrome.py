0a20ccfe2b02fb84a843e97fbe2d3f6d649dfcd0
차지원 <chajiwon100785@gmail.com>
2020-08-17 09:03:19 +0900

    LeetCode : Longest Palindrome

---
+++ LeetCode/20_08_2/Longest Palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        resultCount = len(s)
+        checkOdd = 0
+        for i in set(s):
+            if s.count(i) % 2:
+                resultCount -= 1
+                checkOdd = 1
+        return resultCount + checkOdd
+
+a = Solution()
+print(a.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"))
