606418ef4dccb9c2261d6b22660d71658d6fcb95
차지원 <chajiwon100785@gmail.com>
2020-09-13 22:01:32 +0900

    LeetCode: Compare Version Numbers

--- LeetCode/20_09_2/Compare Version Numbers.py
+++ LeetCode/20_09_2/Compare Version Numbers.py
@@ -1,20 +1,13 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        ver1 = version1.split(".")
-        ver2 = version2.split(".")
-
-        i = 0
-        while i < min(len(ver1), len(ver2)):
-            if int(ver1[i]) > int(ver2[i]):
-                return 1
-            if int(ver1[i]) < int(ver2[i]):
-
-                return -1
-            i += 1
-        if len(ver1) > len(ver2) and int(ver1[-1]) > 0:
-            return 1
-        elif len(ver1) < len(ver2) and int(ver2[-1]) > 0:
-            return -1
+        ver1 = [int(i) for i in version1.split(".")]
+        ver2 = [int(i) for i in version2.split(".")]
+        for i in range(min(len(ver1), len(ver2))):
+            if ver1[i] > ver2[i]: return 1
+            if ver1[i] < ver2[i]: return -1
+        if set(ver1[i+1:]) - {0}:return 1
+        if set(ver2[i+1:]) - {0}:return -1
         return 0
+
 a = Solution()
-print(a.compareVersion("1.1.0","1.1"))
+print(a.compareVersion("1.1.0.0.0","1.1"))
