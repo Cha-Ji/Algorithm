7b917bb1f5724ac74e496e26681f8fd99612bda4
차지원 <chajiwon100785@gmail.com>
2021-01-25 04:25:53 +0900

    Ex_2879 코딩은 예쁘게 그리디 [골드1] 너무 어렵..ㅠ

--- Backjoon/21_01_04_greedy/2879.py
+++ Backjoon/21_01_04_greedy/2879.py
@@ -1,4 +1,9 @@
+# 탭 작업에 대한 다른 이해:
+# 1. 그룹 평준화
+# 2. 그룹 전체 탭
+
 # Ex_2879 코딩은 예쁘게 [골드1]
+import heapq
 N = int(input())
 thisTab = list(map(int, input().split()))
 correctTab = list(map(int, input().split()))
@@ -7,7 +12,14 @@ diffTab = [0]*N
 for i in range(N):
     diffTab[i] = correctTab[i] - thisTab[i]
 
-# 그룹 중 가장 작은 수를 일괄 차감
-group = []
+tab = diffTab[0]
+ans = 0
 
-# 차감 후 안에서 다시 그룹화
+for i in range(1, N):
+    if tab * diffTab[i] < 0:  # 그룹 단체 탭
+        ans += abs(tab)
+    elif abs(tab) > abs(diffTab[i]):  # 그룹 평준화
+        ans += abs(tab - diffTab[i])
+    tab = diffTab[i]
+
+print(ans + abs(tab))
