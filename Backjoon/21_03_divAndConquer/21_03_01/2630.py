1f6841fd35c3cbdf47468f8c62726981e64aeb89
차지원 <chajiwon100785@gmail.com>
2021-03-05 18:16:20 +0900

    Ex_2630 색종이 만들기: 분할정복 [실3]

--- Backjoon/21_03_divAndConquer/21_03_01/2630.py
+++ Backjoon/21_03_divAndConquer/21_03_01/2630.py
@@ -1,52 +1,17 @@
 # Ex_2630 색종이 만들기 [실3]
-def isOneColor(start, width, ans, color):
-    # 정지조건
-    if width == 1:
-        ans[color] += 1
-        return ans
-    # 통일 여부
-    correct = True
-    for i in range(width - 1):
-        for j in range(width - 1):
-            if board[start[0] + i][start[1] + j] != color:
-                correct = False
-                break
+def sol(x, y, w, ans):
+    if w * w * board[x][y] == sum(sum(board[x + i][y: y + w]) for i in range(w)):
+        ans[board[x][y]] += 1
 
-    if color == 1 and correct:
-        print(start, width, ans)
-    # 통일되었다면 ans 증가
-    if correct:
-        ans[color] += 1
-
-    # 통일되지 않았다면 4분할 재귀
     else:
-        width //= 2
-        for dx, dy in (0, 0), (0, width - 1), (width - 1, 0), (width - 1, width - 1):
-            start[0] += dx
-            start[1] += dy
-            nextColor = board[start[0]][start[1]]
-            ans = isOneColor(start, width, ans, nextColor)
+        w //= 2
+        for dx, dy in (0, 0), (0, w), (w, 0), (w, w):
+            ans = sol(x + dx, y + dy, w, ans)
     return ans
 
 
 N = int(input())
-board = [[0] * N for _ in range(N)]
-for i in range(N):
-    board[i] = list(map(int, input().split()))
-
-ans = isOneColor([0, 0], N, [0, 0], board[0][0])
-
+board = [list(map(int, input().split()))for _ in range(N)]
+ans = sol(0, 0, N, [0, 0])
 print(ans[0])
 print(ans[1])
-"""
-if not isOneColor([0, 0], N):
-    if not isOneColor([0, 0], N/2):
-        adf
-    isOneColor([N/2, 0], N/2)
-    isOneColor([0, N/2], N/2)
-    isOneColor([N/2, N/2], N/2)
-
-
-
-
-"""
