fabe1d2da747463e5cb72fb96b438b451491bc0c
차지원 <chajiwon100785@gmail.com>
2021-03-10 18:26:59 +0900

    Ex_2042 구간합 구하기: 세그트리[골1] 아직 미숙한듯,,

--- Backjoon/21_03_divAndConquer/21_03_01/2042.py
+++ Backjoon/21_03_divAndConquer/21_03_01/2042.py
@@ -1,33 +1,32 @@
 # Ex_2042 구간 합 구하기 [골1]
 import sys
-import math
 input = sys.stdin.readline
 
 
 def init(node, s, e):
     if s == e:
         tree[node] = num[s]
-        return tree[node]
-
-    return init(node * 2, s, (s + e) // 2) + init(node * 2 + 1, (s + e) // 2 + 1, e)
+    else:
+        tree[node] = init(node * 2, s, (s + e) // 2) + \
+            init(node * 2 + 1, (s + e) // 2 + 1, e)
+    return tree[node]
 
 
 def subset(node, b, c, l, r):
-    if l <= b and c <= r:
+    if b <= l and r <= c:
         return tree[node]
-    elif r > b or c < l:
+    elif b > r or c < l:
         return 0
     else:
         return subset(node * 2, b, c, l, (l+r) // 2) + subset(node * 2 + 1, b, c, (l+r)//2 + 1, r)
 
 
 def change(node, b, c, l, r):
-    if l == r:
-        tree[node] += c - b
-    elif l <= b <= r:
-        tree[node] += c - b
-        change(node * 2, b, c, l, (l + r) // 2)
-        change(node * 2 + 1, b, c, (l + r) // 2 + 1, r)
+    if l <= b <= r:
+        tree[node] += c
+        if l != r:
+            change(node * 2, b, c, l, (l + r) // 2)
+            change(node * 2 + 1, b, c, (l + r) // 2 + 1, r)
 
 
 N, M, K = map(int, input().split())
@@ -38,5 +37,7 @@ tree = [0] * pow(2, 21)  # 2^(log N + 1)
 init(1, 0, N-1)
 for _ in range(M + K):
     a, b, c = map(int, input().split())
-
-    change(1, b, c, 0, N-1) if a == 1 else print(subset(1, b, c, 0, N-1))
+    if a == 1:
+        node = subset(1, b-1, b-1, 0, N - 1)
+    change(1, b-1, c - node, 0, N -
+           1) if a == 1 else print(subset(1, b-1, c-1, 0, N-1))
