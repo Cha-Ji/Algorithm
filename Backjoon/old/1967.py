ac9b853fd0963e9a652f2f0d78e5efdbc51eb985
차지원 <chajiwon100785@gmail.com>
2021-03-17 22:42:55 +0900

    Fix: del old

--- Backjoon/old/1967.py
+++
@@ -1,9 +0,0 @@
-#Ex_1967
-#tree
-# diameter
-import sys
-for _ in range(int(sys.stdin.readline())):
-    input_num = list(map(int,sys.stdin.readline().split()))
-    parent = input_num[0]
-    child = input_num[1]
-    weight = input_num[2]
