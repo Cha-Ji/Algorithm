22ab1f5bbaedc7fd701c54cf85b29bd6b18427f3
차지원 <chajiwon100785@gmail.com>
2021-03-19 06:59:10 +0900

    Ex_1655 가운데를 말해요: 우선순위 큐[골2]

--- clear/1655.py
+++ clear/1655.py
@@ -1,22 +1,25 @@
-#Ex_1655
+# Ex_1655
 # priority Queue
 # import heapq, tuple
 import sys
 import heapq
-minH,maxH=[],[]
-for _ in range(int(sys.stdin.readline())):
-    m=int(sys.stdin.readline())
-    if len(minH)==len(maxH):
-        heapq.heappush(maxH,(-m,m))
-        #heappush = minHeap
+input = sys.stdin.readline
+
+minH, maxH = [10001], [10001]
+plusMax = True
+for i in range(int(input())):
+    if plusMax:
+        heapq.heappush(maxH, -int(input()))
     else:
-        heapq.heappush(minH,m)
-    if (len(minH)and len(maxH))and maxH[0][1] > minH[0]:
-        a=heapq.heappop(maxH)[1]
-        b=heapq.heappop(minH)
-        heapq.heappush(minH,a)
-        heapq.heappush(maxH,(-b,b))
-    print(maxH[0][1])
+        heapq.heappush(minH, int(input()))
 
+    plusMax = not plusMax
+    # 최대힙의 모든 원소는 최소힙의 모든 원소보다 작아야한다.
+    # 서로의 최솟값을 주고받으며 유지시킨다.
+    if -maxH[0] > minH[0]:
+        a = -heapq.heappop(maxH)
+        b = heapq.heappop(minH)
+        heapq.heappush(minH, a)
+        heapq.heappush(maxH, -b)
 
-
+    print(-maxH[0])
