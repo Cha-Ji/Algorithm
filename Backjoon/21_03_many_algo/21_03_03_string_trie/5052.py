d482c4c585b2e9d09819763f9f5991c697094be3
차지원 <chajiwon100785@gmail.com>
2021-03-22 07:39:17 +0900

    Ex_5052 전화번호 목록: trie [골4]

---
+++ Backjoon/21_03_many_algo/21_03_03_string_trie/5052.py
@@ -0,0 +1,55 @@
+# Ex_5052 전화번호 목록 [골4]
+import sys
+input = sys.stdin.readline
+
+
+class Node():
+    def __init__(self, key):
+        self.key = key
+        self.child = [-1] * 11
+
+
+class Trie():
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, phoneNum):
+        cur = self.head
+
+        for num in phoneNum:
+            num = int(num)
+            if cur.child[num] == -1:
+                cur.child[num] = Node(num)
+            cur = cur.child[num]
+
+        # 끝맺음
+        cur.child[num] = -2
+
+    def search(self, phoneNum):
+        cur = self.head
+
+        for num in phoneNum:
+            num = int(num)
+
+            if -2 in cur.child:
+                return False
+
+            cur = cur.child[num]
+        return True
+
+
+for _ in range(int(input())):
+    trie = Trie()
+
+    n = int(input())
+    phoneBook = [input()[:-1] for _ in range(n)]
+
+    for num in phoneBook:
+        trie.insert(num)
+
+    ans = "YES"
+    for num in phoneBook:
+        if not trie.search(num):
+            ans = "NO"
+
+    print(ans)
