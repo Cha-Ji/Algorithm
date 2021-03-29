9f4d94e938a96a1319f5b7761e2a96fbe1740f99
차지원 <chajiwon100785@gmail.com>
2021-03-29 20:21:23 +0900

    Ex_16903 수열과 쿼리 20: trie, bin, xor[플2]

---
+++ Backjoon/21_03_many_algo/21_03_04_KMP_String/16903.py
@@ -0,0 +1,66 @@
+# Ex_16903 수열과 쿼리 20 [플2]
+import sys
+input = sys.stdin.readline
+
+
+def insert():
+    cur = A
+    for b in binX:
+        b = int(b)
+        if cur[b]:
+            # count ++
+            cur[b][2] += 1
+        else:
+            cur[b] = [False, False, 0]
+
+        cur = cur[b]
+
+
+def delete():
+    cur = A
+    for b in binX:
+        b = int(b)
+        if cur[b][2] > 0:
+            # count --
+            cur[b][2] -= 1
+        else:
+            cur[b] = False
+            break
+
+        cur = cur[b]
+
+
+def xor():
+    ans = "0b"
+
+    cur = A
+    for b in binX:
+        b = int(b)
+        if cur[1 - b]:
+            # b ^ cur == 1이 존재
+            ans += '1'
+            cur = cur[1 - b]
+        else:
+            # b ^ cur == 1이 불가
+            ans += '0'
+            cur = cur[b]
+
+    print(int(ans, 2))
+
+
+# 0, 1, count
+A = [False, False, 0]
+
+binX = format(0, 'b').zfill(30)
+insert()
+
+for i in range(int(input())):
+    order, x = map(int, input().split())
+    binX = format(x, 'b').zfill(30)
+
+    if order == 1:
+        insert()
+    elif order == 2:
+        delete()
+    else:
+        xor()
