53d3fc946460d5f159c50f04c572404cf6bf5a8a
Cha_Ji <chajiwon1007@pukyong.ac.kr>
2021-05-27 13:34:22 +0900

    Ex_1799 비숍: backtrack [골2]

--- Backjoon/21_04_str_backT/21_04_02_backtrack/1799.py
+++ Backjoon/21_04_str_backT/21_04_02_backtrack/1799.py
@@ -1,44 +1,41 @@
 # Ex_1799 비숍: [골2]
+def dfs(i, count, wb):
+    if i >= N + wb:
+        return
 
+    ans[0] = max(ans[0], count)
 
-def dfs(diffXY, cnt):
-    # y - x로 대각선 그룹을 나눈다.
-    # 우상단 => 좌하단
-    if abs(diffXY) < N:
-        ans[0] = max(ans[0], cnt)
+    if wb == 0:
+        canVisit = white
+    else:
+        canVisit = black
 
-        # 현재 대각선 요소 중 하나 방문
-        # v = 반대방향 대각선 방문
-        for x in range(max(0, - diffXY), min(N, N - diffXY)):
-            y = diffXY + x
-            if board[x][y] == 1 and not v[x + y]:
-                v[x + y] = True
-                dfs(diffXY + 1, cnt + 1)
-                v[x + y] = False
-        dfs(diffXY + 1, cnt)
-
-# def dfs():
-#     stack = []
-#     diff = -N + 1
-#     stack.append([diff, []])
-
-#     while abs(diff) < N:
-#         diff, v = stack.pop()
-#         ans[0] = max(ans[0], len(v))
-
-#         stack.append([diff + 1, v])
-#         for x in range(max(0, - diff), min(N, N - diff)):
-#             y = diff + x
-#             if board[x][y] == 1 and x + y not in v:
-#                 stack.append([diff + 1, v + [x + y]])
-
-
+    for x, y in canVisit[i]:
+        if not visited[x - y + N]:
+            visited[x - y + N] = True
+            dfs(i + 1, count + 1, wb)
+            visited[x - y + N] = False
+    # 미방문
+    dfs(i + 1, count, wb)
 
 
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
-v = [False] * 2 * N
-ans = [0]
+visited = [False] * 2 * N
+white = [[] for _ in range(N + 1)]
+black = [[] for _ in range(N + 1)]
 
-dfs()
-print(ans)
+for x in range(N):
+    for y in range(N):
+        if board[x][y] == 1:
+            if (x + y) % 2 == 0:
+                white[(x + y)//2].append([x, y])
+            else:
+                black[(x + y)//2].append([x, y])
+
+ans = [0]
+dfs(0, 0, 0)
+dfs(0, ans[0], 1)
+
+print(*ans)
+
