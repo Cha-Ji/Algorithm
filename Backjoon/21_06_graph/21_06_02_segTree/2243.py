0e6a86ad7bb874937d2115195be902126611a992
차지원 <chajiwon100785@gmail.com>
2021-12-09 22:29:38 +0900

    알고리즘 재개하기 전 정리

---
+++ Backjoon/21_06_graph/21_06_02_segTree/2243.py
@@ -0,0 +1,34 @@
+# Ex_2243 사탕상자: [플5]
+
+def addCandy(favor, num, index):
+    if tree[index][1] == -1:
+        tree[index] = [0, favor]
+        return
+
+    if tree[index][1] < favor:
+        tree[index][0] += num
+        tree[index][1] = favor
+        addCandy(favor, num, index * 2 + 1)
+    
+    else:
+        tree[index][0] += num
+        addCandy(favor, num, index * 2)
+
+def delCandy(favor, num, index):
+    if tree[index][1] < favor:
+        tree[index][0] -= num
+        delCandy(favor, index * 2 + 1)
+    
+
+n = int(input())
+tree = [[0, -1] for _ in range(n)] # [자식 개수, 최대값]
+for _ in range(n):
+    order = list(map(int, input().split()))
+    
+    # 사탕을 꺼내는 경우
+    if order[0] == 1:
+        delCandy(order[1], 0, 1)
+    
+    # 사탕을 넣는 경우
+    if order[1] == 2:
+        addCandy(order[1], order[2], 1)
---
+++ Backjoon/21_07_/21_07_01_sort_string/18870.py
@@ -0,0 +1,44 @@
+# Ex_18870 좌표 압축: [실2] 머지소트
+def merge(left, right):
+    result = []
+    l, r = 0, 0
+    lenL, lenR = len(left), len(right)
+
+    while l < lenL and r < lenR:
+        # 정복
+        if left[l] <= right[r]:
+            result.append(left[l])
+            l += 1
+        else:
+            result.append(right[r])
+            r += 1
+
+    # 한 쪽의 정렬이 끝났을 때
+    if l < lenL: result += left[l:]
+    if r < lenR: result += right[r:]
+
+    return result
+
+
+def mergeSort(lst):
+    if len(lst) <= 1:
+        return lst
+
+    mid = len(lst) // 2
+    l = mergeSort(lst[:mid])
+    r = mergeSort(lst[mid:])
+    return merge(l, r)
+
+
+answer = [0] * int(input())
+x = list(map(int, input().split()))
+newX = list(set(x))  # 중복 제거
+sortedX = mergeSort(newX)  # 소팅
+
+dictX = {}  # 매핑
+for i in range(len(sortedX)):
+    dictX[sortedX[i]] = i
+
+for i in range(len(x)):
+    answer[i] = dictX[x[i]]
+print(*answer)
